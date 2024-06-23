3757680f29d43ad2b8afc00e5ed6a09426e4e135
Anton Bilous <anton.bilosus@gmail.com>
2024-06-23 21:36:45 +0300

    Add __str__ methods

--- taxi/models.py
+++ taxi/models.py
@@ -11,8 +11,14 @@ class Manufacturer(models.Model):
     name = models.CharField(max_length=63, unique=True)
     country = models.CharField(max_length=63)
 
+    def __str__(self):
+        return f"{self.name} ({self.country})"
+
 
 class Car(models.Model):
     model = models.CharField(max_length=63)
     manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
     drivers = models.ManyToManyField(settings.AUTH_USER_MODEL)
+
+    def __str__(self):
+        return f"{self.model} ({self.manufacturer.name})"
