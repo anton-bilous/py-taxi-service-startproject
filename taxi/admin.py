06f1b44066852a696b4fae57599056170246b922
Anton Bilous <anton.bilosus@gmail.com>
2024-06-23 21:22:41 +0300

    Configure admin panel

--- taxi/admin.py
+++ taxi/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from taxi.models import Driver, Car, Manufacturer
+
+
+@admin.register(Driver)
+class DriverAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("license_number",)
+    extra_fieldsets = (("Additional info", {"fields": ("license_number",)}),)
+    fieldsets = UserAdmin.fieldsets + extra_fieldsets
+    add_fieldsets = UserAdmin.add_fieldsets + extra_fieldsets
+
+
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    search_fields = ("model",)
+    list_filter = ("manufacturer",)
+
+
+admin.site.register(Manufacturer)
