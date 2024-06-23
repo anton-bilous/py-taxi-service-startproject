1c79ee0717ab5d2ac998653189f890b5cc51dcf9
Anton Bilous <anton.bilosus@gmail.com>
2024-06-23 21:39:24 +0300

    Add Meta classes

--- taxi/models.py
+++ taxi/models.py
@@ -11,6 +11,9 @@ class Manufacturer(models.Model):
     name = models.CharField(max_length=63, unique=True)
     country = models.CharField(max_length=63)
 
+    class Meta:
+        ordering = ("name",)
+
     def __str__(self):
         return f"{self.name} ({self.country})"
 
@@ -19,6 +22,9 @@ class Car(models.Model):
     model = models.CharField(max_length=63)
     manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
     drivers = models.ManyToManyField(settings.AUTH_USER_MODEL)
+
+    class Meta:
+        ordering = ("model",)
 
     def __str__(self):
         return f"{self.model} ({self.manufacturer.name})"
